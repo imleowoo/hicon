5564d9458af9031fbef9d714381c912dcfe67300
wulei <imleowoo@outlook.com>
2024-05-30 23:32:13 +0800

    feat: 新增命令行支持

--- hicon/main.py
+++ hicon/main.py
@@ -24,18 +24,31 @@ async def page_content(url: str, headless: bool = True) -> str:
         await browser.close()
 
 
-async def main():
+def extract(content: str) -> dict:
+    """提取页面正文
+
+    :param content: 网页源代码
+    :type content: str
+    """
+    extractor = GeneralNewsExtractor()
+    result = extractor.extract(content)
+    return result
+
+
+def main():
     parser = ArgumentParser('通过URL获取正文信息')
     parser.add_argument('url', type=str, help='需要提取正文的页面URL')
     parser.add_argument('--browser-headless', type=bool, default=1, help='浏览器是否无头')
     args = parser.parse_args()
-    # content = await page_content('https://news.ifeng.com/c/8ZgrBRofRh3')
-    content = await page_content(url=args.url, headless=args.browser_headless)
-    extractor = GeneralNewsExtractor()
-    result = extractor.extract(content)
-    pprint(result)
-    return result
+
+    async def execute():
+        # content = await page_content('https://news.ifeng.com/c/8ZgrBRofRh3')
+        content = await page_content(url=args.url, headless=args.browser_headless)
+        result = extract(content)
+        pprint(result)
+
+    asyncio.run(execute())
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
